c7b47a19e42afaab30b8f7f286c624550890cfec
AndrewNVH <andrew.narendra.virija@gmail.com>
2024-05-10 22:44:11 +0700

    OpeningApp

--- app/src/main/java/com/example/mdp_project/MainActivity.kt
+++ app/src/main/java/com/example/mdp_project/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.mdp_project
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import androidx.activity.enableEdgeToEdge
+import com.example.mdp_project.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var  binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        enableEdgeToEdge()
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler().postDelayed({
+            val intent = Intent(this@MainActivity, Start::class.java)
+            startActivity(intent)
+            finish()
+            },1800)
+
     }
+    override fun onDestroy() {
+        super.onDestroy()
+        }
 }
