1a52cbdd441046fd3eb5b5309cdb097fc2ad81eb
AndrewNVH <andrew.narendra.virija@gmail.com>
2024-06-15 17:18:26 +0700

    RegisterViewModel, RegisterFragment... masih Error soale khusus Repo menurutku...
    Masih ta tarok MockDB, entah kenapa error dan ChatGPT nda membantu... nanti gampang tinggal di masukkan ke mariaDB haruse

--- app/src/main/java/com/example/mdp_project/DataClass.kt
+++ app/src/main/java/com/example/mdp_project/DataClass.kt
@@ -2,3 +2,8 @@ package com.example.mdp_project
 
 data class MemberList(val memberName: String, val memberGroup: String, val logo: Int)
 data class UserList(val userName: String, val password: String)
+data class RegisterList(val userName: String, val Password:String, val cPassword:String)
+
+data class Msg(
+    var text: String
+)
--- app/src/main/java/com/example/mdp_project/MockDB.kt
+++ app/src/main/java/com/example/mdp_project/MockDB.kt
@@ -3,6 +3,7 @@ package com.example.mdp_project
 object MockDB{
     val member = ArrayList<MemberList>();
     val user = ArrayList<UserList>();
+    val register = ArrayList<RegisterList>(); // Permisalan pake MockDB
 
 //    val member:ArrayList<MemberList> = arrayListOf(
 //        MemberList("udfsdf", "sidnfis", 8)
